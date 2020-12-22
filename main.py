8b58022941fba39fea09097f86871088e7d60c56
adrien <adri_00@hotmail.fr>
2020-12-22 09:30:52 +0100

    🚀 RELEASE: structuration package

---
+++ main.py
@@ -0,0 +1,5 @@
+from model_rpg import Personnage
+
+
+test = Personnage('Mike', 1, 'France', 1)
+print(test)
---
+++ utils/__init__.py
@@ -0,0 +1,11 @@
+TYPE_ETHNIE = {
+    "Congo": 1,
+    "Mongolie": 2,
+    "France": 3,
+    "USA": 4,
+    "Japon": 5,
+    "Cote_Ivoire": 6,
+    "Argentine": 7,
+    "Haiti": 8,
+    "Angleterre": 9
+}
